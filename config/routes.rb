a3901fe8952c10ef1920d8312006c32e41147724
ShuopengLiu <shuopengliu@gmail.com>
2021-03-11 00:47:58 -0600

    Re-generated home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  get 'parks/index'
-  get 'parks/show'
-  get 'neighbourhoods/index'
-  get 'neighbourhoods/show'
-  get 'categories/index'
-  get 'categories/show'
-  get 'trees/index'
-  get 'trees/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  # get 'parks/index'
+  # get 'parks/show'
+  # get 'neighbourhoods/index'
+  # get 'neighbourhoods/show'
+  # get 'categories/index'
+  # get 'categories/show'
+  # get 'trees/index'
+  # get 'trees/show'
+  root to: "home#index"
+  resources :trees, only: %i[index show]
+  resources :categories, only: %i[index show]
+  resources :neighbourhoods, only: %i[index show]
+  resources :parks, only: %i[index show]
+
 end
